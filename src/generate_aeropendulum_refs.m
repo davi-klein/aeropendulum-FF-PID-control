ec649a645d3ea2c0753a5c71d73640c17cbf9333
davi-klein <davihklein83@gmail.com>
2026-07-07 17:34:27 -0300

    refactor: added type as an arg to save the trajectory timeseries in a single variable (user choice between sine and trapezoidal)

--- src/generate_aeropendulum_refs.m
+++ src/generate_aeropendulum_refs.m
@@ -1,5 +1,6 @@
-function [ts_sine, ts_trap] = generate_aeropendulum_refs(t_end, dt)
+function [th_ref_ts] = generate_aeropendulum_refs(type, t_end, dt)
     % GENERATE_AEROPENDULUM_REFS Generates smooth reference trajectories
+    % type: type of trajectory (accepted args: 'sine' or 'trapezoidal')
     % t_end: Total simulation time
     % dt: Sample time
     
@@ -13,31 +14,33 @@ function [ts_sine, ts_trap] = generate_aeropendulum_refs(t_end, dt)
     t_hold = 2;         % Hold duration at top and bottom
     t_cycle = 2*t_trans + 2*t_hold; % Total time for one full trapezoid cycle
     
-    % 1. Sine Wave Reference
-    th_sine = (amp/2) - (amp/2)*cos(2*pi*freq*t);
-    
-   % 2. Differentiable Trapezoidal Reference
-    th_trap = zeros(size(t));
-    
-    for i = 1:length(t)
-        t_c = mod(t(i), t_cycle); 
+    if strcmp('sine',type)
+        % 1. Sine Wave Reference
+        th_sine = (amp/2) - (amp/2)*cos(2*pi*freq*t);
+        th_ref_ts = timeseries(th_sine', t');
+    elseif strcmp('trapezoidal',type)
+       % 2. Differentiable Trapezoidal Reference
+        th_trap = zeros(size(t));
         
-        if t_c < t_trans
-            th_trap(i) = (amp/2) * (1 - cos(pi * t_c / t_trans));
+        for i = 1:length(t)
+            t_c = mod(t(i), t_cycle); 
             
-        elseif t_c < (t_trans + t_hold)
-            th_trap(i) = amp;
-            
-        elseif t_c < (2*t_trans + t_hold)
-            t_fall = t_c - (t_trans + t_hold);
-            th_trap(i) = (amp/2) * (1 + cos(pi * t_fall / t_trans));
-            
-        else
-            th_trap(i) = 0;
+            if t_c < t_trans
+                th_trap(i) = (amp/2) * (1 - cos(pi * t_c / t_trans));
+                
+            elseif t_c < (t_trans + t_hold)
+                th_trap(i) = amp;
+                
+            elseif t_c < (2*t_trans + t_hold)
+                t_fall = t_c - (t_trans + t_hold);
+                th_trap(i) = (amp/2) * (1 + cos(pi * t_fall / t_trans));
+                
+            else
+                th_trap(i) = 0;
+            end
         end
+        th_ref_ts = timeseries(th_trap', t');
+    else
+        disp('Unidentified Trajectory.')
     end
-    
-    % Create timeseries objects for Simulink
-    ts_sine = timeseries(th_sine', t');
-    ts_trap = timeseries(th_trap', t');
 end
