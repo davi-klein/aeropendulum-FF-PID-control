9a1de84dab93e841a3d6b3eab403903b68bb7ee4
davi-klein <davihklein83@gmail.com>
2026-07-07 17:37:24 -0300

    refactor: changed main orchestrator to include the optimization process

--- main_aeropendulum.m
+++ main_aeropendulum.m
@@ -6,23 +6,34 @@ clc; close all; clear; format short eng;
 addpath('src');
 
 disp('Loading physical parameters...');
-params = init_aeropendulum_params();
+p = init_aeropendulum_params();
+assignin('base', 'p', p)
 
 disp('Generating Trajectories...');
-[ts_sine, ts_trap] = generate_aeropendulum_refs(20, 0.001);
+th_ref_ts = generate_aeropendulum_refs(p.type, p.stpt, p.dt);
 
 disp('Configuring Simulink environment...');
 model_name = 'aeropendulum_dynamics';
 load_system(model_name);
 
-% Create a simulation input object
+disp('======================================================');
+disp('-------------Starting Firefly Optimization------------');
+disp('======================================================');
+% Pass the sine wave for tuning 
+[K_opt, f_evals, exec_time] = FMA(model_name, th_ref_ts);
+
+fprintf('\n=== Optimization Results ===\n');
+fprintf('Kp = %.4f\nKi = %.4f\nKd = %.4f\n', K_opt(1), K_opt(2), K_opt(3));
+fprintf('----------------------------\n');
+fprintf('Function Evaluations : %d\n', f_evals);
+fprintf('Total Execution Time : %.2f seconds\n', exec_time);
+fprintf('Time per Evaluation  : %.4f seconds\n\n', exec_time / f_evals);
+
+disp('Simulating optimal response...');
+
 simIn = Simulink.SimulationInput(model_name);
-
-% Inject the 'params' struct safely into the simulation workspace
-simIn = simIn.setVariable('p', params);
-
-disp('Running Simulation...');
-simOut = sim(simIn);
-
-disp('Simulation finished successfully.');
-% Here you can plot your results using simOut (e.g., simOut.tout)
+simIn = simIn.setVariable('Kp', K_opt(1));
+simIn = simIn.setVariable('Ki', K_opt(2));
+simIn = simIn.setVariable('Kd', K_opt(3));
+simIn = simIn.setVariable('th_ref', th_ref_ts);
+out = sim(simIn);
