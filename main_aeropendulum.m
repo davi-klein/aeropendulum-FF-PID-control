9ab657e5e47ddde070d389bbc73474b5de23831c
davi-klein <davihklein83@gmail.com>
2026-07-08 20:53:24 -0300

    refactor: updated main to run post_process_results.m and turn simulink warnings through the optimization process

--- main_aeropendulum.m
+++ main_aeropendulum.m
@@ -9,7 +9,7 @@ disp('Loading physical parameters...');
 p = init_aeropendulum_params();
 assignin('base', 'p', p)
 
-disp('Generating Trajectories...');
+fprintf('Generating %s trajectory...', p.type);
 th_ref_ts = generate_aeropendulum_refs(p.type, p.stpt, p.dt);
 
 disp('Configuring Simulink environment...');
@@ -19,8 +19,13 @@ load_system(model_name);
 disp('======================================================');
 disp('-------------Starting Firefly Optimization------------');
 disp('======================================================');
+
+warning('off', 'Simulink:Commands:SimulationsWithErrors');
+
 % Pass the sine wave for tuning 
 [K_opt, f_evals, exec_time] = FMA(model_name, th_ref_ts);
+
+warning('on', 'Simulink:Commands:SimulationsWithErrors');
 
 fprintf('\n=== Optimization Results ===\n');
 fprintf('Kp = %.4f\nKi = %.4f\nKd = %.4f\n', K_opt(1), K_opt(2), K_opt(3));
@@ -29,11 +34,9 @@ fprintf('Function Evaluations : %d\n', f_evals);
 fprintf('Total Execution Time : %.2f seconds\n', exec_time);
 fprintf('Time per Evaluation  : %.4f seconds\n\n', exec_time / f_evals);
 
-disp('Simulating optimal response...');
+% Simulate and plot optimal response
+opt_metrics.f_evals = f_evals;
+opt_metrics.exec_time = exec_time;
+opt_metrics.time_per_eval = exec_time / f_evals;
 
-simIn = Simulink.SimulationInput(model_name);
-simIn = simIn.setVariable('Kp', K_opt(1));
-simIn = simIn.setVariable('Ki', K_opt(2));
-simIn = simIn.setVariable('Kd', K_opt(3));
-simIn = simIn.setVariable('th_ref', th_ref_ts);
-out = sim(simIn);
+post_process_results(p.type, model_name, K_opt, th_ref_ts, opt_metrics)
